c9aacb9cb19e3ca323e52d9eec8cdda18849265d
gladius <garthsnyder@protonmail.com>
2022-12-13 15:35:09 -0500

    refactor

--- Movies/UI/MovieList/MovLstVC.swift
+++ Movies/UI/MovieList/MovLstVC.swift
@@ -1,36 +1,26 @@
 import UIKit
 
 class MovieListViewController: UIViewController, ActivityIndicatorContainer {
-    
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     @IBOutlet weak var tableView: UITableView!
     let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+    private lazy var dataSource = MovieListDataSource(delegate: self)
     private let viewModel = MovieListViewModel()
-    lazy var dataSource = MovieListDataSource(delegate: self)
-    var arbitraryExtraParameterToMakeConvenienceInitWork = ""   /// * why needed? ... also ... try uncommenting the below required init?(coder: ) , and see what happens!
-    
-    convenience init?(coder: NSCoder, para: String = "") {
-        self.init(coder: coder)
-        self.arbitraryExtraParameterToMakeConvenienceInitWork = para
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupNavigationBar(withTitle: Constants.uiTitleString)
-        setupMovieListUI()
-        viewModel.bind(to: self)
-        updateMoviesListUIAndFetch(withMovieTypeString: Constants.apiString)
-    }
-    
-    func updateMoviesListUIAndFetch(withMovieTypeString movieTypeString: String) {
-        activityIndicator.startAnimating()
         tableView.isHidden = true
-        viewModel.serviceFetchMovies(withMovieTypeString: movieTypeString)
+        activityIndicator.startAnimating()
+        setupNavigationBar(withTitle: Constants.uiTitleString)
+        setupTableView()
+        viewModel.bind(to: self)
+        viewModel.serviceFetchMovies()
     }
     
-    func setupMovieListUI() {
+    private func setupTableView() {
         tableView.rowHeight = 200
-        tableView.register(UINib(nibName: "MovieListCell", bundle: nil), forCellReuseIdentifier: "MovieListCell") /// use a protocol rather than hardcoding
+        tableView.register(UINib(nibName: "MovieListCell", bundle: nil),
+                           forCellReuseIdentifier: "MovieListCell") /// use a protocol rather than hardcoding
         tableView.dataSource = dataSource
         tableView.delegate = dataSource
     }
@@ -38,13 +28,10 @@ class MovieListViewController: UIViewController, ActivityIndicatorContainer {
 
 extension MovieListViewController: MovieListViewModelDelegate {
     func didFetchMovies(_ movies: [Movie]) {
+        tableView.isHidden = false
         activityIndicator.stopAnimating()
         dataSource.movies = movies
-        tableView.isHidden = false
         tableView.reloadData()
     }
 }
 
-//    required init?(coder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
--- Movies/UI/MovieList/MovieListViewModel.swift
+++ Movies/UI/MovieList/MovieListViewModel.swift
@@ -7,23 +7,19 @@ protocol MovieListViewModelDelegate: ErrorReceivableDelegate {
 
 
 class MovieListViewModel {
-    
-    private let service: MovieListServiceProtocol
-    
     private weak var delegate: MovieListViewModelDelegate?
-    
+    private let service: MovieListServiceProtocol
     
     init(service: MovieListServiceProtocol = MovieListService()) {
         self.service = service
     }
     
-    
     func bind(to delegate: MovieListViewModelDelegate) {
         self.delegate = delegate
     }
     
-    func serviceFetchMovies(withMovieTypeString str: String) {
-        service.fetchMovies(withMovieTypeString: str) { [weak self] (result) in
+    func serviceFetchMovies() {
+        service.fetchMovies() { [weak self] (result) in
             switch result {
             case .success(let movieList):
                 let movies = movieList.results
