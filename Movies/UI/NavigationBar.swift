0f1f1975d0a346b77ecbfb6c56f467d461cad972
gladius <garthsnyder@protonmail.com>
2022-12-14 01:30:00 -0500

    rename / refactor

--- Movies/UI/NavigationBar.swift
+++ Movies/UI/NavigationBar.swift
@@ -7,7 +7,7 @@ extension UIViewController {
     func setupNavigationBar(withTitle title: String) {
         self.title = title
         
-        print("setting up navigation bar with title \(title)")
+        print("\nsetting up navigation bar with title \(title)")
         
         navigationItem.largeTitleDisplayMode = .never // .always
     }
